fd3d9ddc754d6f5124047385ca72bff9f11245b6
Jose Orantes <jmorante@ucsc.edu>
2023-05-16 22:47:10 -0700

    adapted menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,10 +18,6 @@ class Menu extends Phaser.Scene {
 
       
       this.flag = this.add.tileSprite(0,0, 720, 640, 'flag').setOrigin(0,0);
-      this.add.text(game.config.width/2 - 330, 50, 'El Salvador Street', menuConfig);
-      menuConfig.fontSize = 35;
-      this.add.text(game.config.width/2 - 220,game.config.height - 150, 'Instructions? Press the Down Key', menuConfig);
-      this.add.text(game.config.width/2 - 200,game.config.height - 105, 'Ready to go? Press UP to start', menuConfig);
       keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
       keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
 
